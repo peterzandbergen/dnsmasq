68cd533854bc6c70b231555f9d7e1e85ce9f12f5
peterzandbergen <Peter.Zandbergen@myhops.com>
2016-02-09 20:16:15 +0100

    Test for push.

--- doc.go
+++ doc.go
@@ -5,5 +5,6 @@ Package dnsmasq contains functions and types for looking up dnsmasq information.
 
 The package implements parsing the dnsmasq.leases file and assigning fixed ip addresses
 based on the mac address of the client.
+
 */
 package dnsmasq
